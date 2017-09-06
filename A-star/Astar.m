37db25ae6e87b8e15dba4f2b7369095e4f0b75a6
apoorvarpi <arpiapoorva@gmail.com>
2017-09-06 09:13:35 +0530

    All paths can now be found

--- A-star/Astar.m
+++ A-star/Astar.m
@@ -2,23 +2,25 @@ MAP = imread('padded.bmp');
 [Height, Width] = size(MAP);
 imshow(imread('map.bmp'));
 %start position, end position
-[x, y] = ginput(2);
-startx = x(1);
-starty = y(1);
-endx = x(2);
-endy = y(2);
+[x y] = ginput(2);
+startx = y(1);
+starty = x(1);
+endx = y(2);
+endy = x(2);
 close all;
 %F(Esimated), G(Travelled),H(Heuristic) matrices declaration
-F = zeros(Height, Width);
-G = F;
-H = G;
+G = zeros(Height, Width);
+H = single(zeros(Height, Width));
+F = single(inf(Height,Width));
 %openset and closed set declaration
-Close = [];
+Close = G;
 Set = [];
-Open = F;
+Close(MAP==0)=1;                  %Adding object-cells to closed matrix
+Open = G;
 %setting parent
-ParentX = F;
-ParentY = F;
+ParentX = zeros(Height, Width);
+ParentY = zeros(Height, Width);
+
 %initialising open, parents, heuristic, gvalues, fvalues
 Open(startx, starty)=1;
 Set = [Set; [startx, starty]];
@@ -41,17 +43,17 @@ ys = [-1, 0, 1, 1, 1, 0, -1, -1];
 flag=1;
 %declaring the answer
 ANS = imread('colour.jpg');
-ANS(starty:starty+1, startx:startx+1, 1) = 0;
-ANS(starty:starty+1, startx:startx+1, 2) = 255;
-ANS(starty:starty+1, startx:startx+1, 3) = 0;
+ANS(startx:startx+1, starty:starty+1, 1) = 0;
+ANS(startx:startx+1, starty:starty+1, 2) = 255;
+ANS(startx:startx+1, starty:starty+1, 3) = 0;
 
-ANS(endy:endy+1, endx:endx+1, 1) = 0;
-ANS(endy:endy+1, endx:endx+1, 2) = 255;
-ANS(endy:endy+1, endx:endx+1, 3) = 0;
+ANS(endx:endx+1, endy:endy+1, 1) = 0;
+ANS(endx:endx+1, endy:endy+1, 2) = 255;
+ANS(endx:endx+1, endy:endy+1, 3) = 0;
 
 %loop begins
 while count>0
-     [currx, curry] = smallest(F, Open, Set);
+    [currx, curry] = smallest(F, Open, Set);
      if currx==endx && curry==endy
          flag=0;
          ANS = path(imread('colour.jpg'), ParentX, ParentY, endx, endy);
@@ -61,19 +63,15 @@ while count>0
      if currx==0&&curry==0
          break;
      end
-     ANS(curry, currx, 1) = 0;
-     ANS(curry, currx, 2) = 0;
-     ANS(curry, currx, 3) = 255;
-     imshow(ANS);
      Open(currx, curry) = 0;
      count = count-1;
-     Close = [Close, [currx, curry]];
+     Close(currx, curry) = 1;
      New = Open;
      for i=1:8
          nx = currx+xs(i);
          ny = curry+ys(i);
-         if nx>0 && nx<=Width && ny>0 && ny<=Height && MAP(ny,nx)==1
-            if ismember([nx, ny], Close)
+         if nx>0 && nx<=Height && ny>0 && ny<=Width 
+            if (Close(nx,ny)==1)
                 continue;
             end;
             if Open(nx,ny)==0
@@ -89,9 +87,9 @@ while count>0
              ParentY(nx,ny) = curry;
              G(nx,ny) = g;
              F(nx,ny) = G(nx,ny)+H(nx,ny);
-             ANS(ny, nx, 1) = 255;
-             ANS(ny, nx, 2) = 0;
-             ANS(ny, nx, 3) = 255;
+             ANS(nx, ny, 1) = 0;
+             ANS(nx, ny, 2) = 0;
+             ANS(nx, ny, 3) = 255;
              imshow(ANS);
          end;
      end; 
@@ -99,13 +97,13 @@ end;
 if flag==1
     disp 'NOT FOUND';
     
-    ANS(starty-2:starty+2, startx-2:startx+2, 1) = 0;
-    ANS(starty-2:starty+2, startx-2:startx+2, 2) = 255;
-    ANS(starty-2:starty+2, startx-2:startx+2, 3) = 0;
+    ANS(startx-2:startx+2, starty-2:starty+2, 1) = 0;
+    ANS(startx-2:startx+2, starty-2:starty+2, 2) = 255;
+    ANS(startx-2:startx+2, starty-2:starty+2, 3) = 0;
 
-    ANS(endy-2:endy+2, endx-2:endx+2, 1) = 0;
-    ANS(endy-2:endy+2, endx-2:endx+2, 2) = 255;
-    ANS(endy-2:endy+2, endx-2:endx+2, 3) = 0;
+    ANS(endx-2:endx+2, endy-2:endy+2, 1) = 0;
+    ANS(endx-2:endx+2, endy-2:endy+2, 2) = 255;
+    ANS(endx-2:endx+2, endy-2:endy+2, 3) = 0;
 end;
 imshow(ANS);
 title('Final');
